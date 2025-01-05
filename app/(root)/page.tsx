15556da2804b52e4e590b198d34ccccbcd7e67b4
aburich <abukio12345@gmail.com>
2025-01-05 01:57:37 -0800

    search-bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import SearchForm from '../../components/SearchForm'
 
-const page = () => {
+
+const Home = async ({ searchParams }: {
+  searchParams: Promise<{query?: string}>
+}) => {
+  const query = (await searchParams).query;
   return (
-    <div>Home</div>
+    <>
+      <section className='pink_container'>
+        <div className='heading'>Pitch Your Startup, <b>connect with enterpreneurs</b></div>
+
+        <p className='sub-heading !max-w-3xl'>
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+
+        <SearchForm query={query}/>
+      </section>
+    </>
   )
 }
 
-export default page
+export default Home
