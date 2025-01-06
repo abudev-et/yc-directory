bb816066c3f887f7f27fc665334538b7d72a3293
aburich <abukio12345@gmail.com>
2025-01-06 09:33:04 -0800

    startup-card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import SearchForm from '../../components/SearchForm'
+import StartupCard from '@/components/StartupCard';
 
 
 const Home = async ({ searchParams }: {
   searchParams: Promise<{query?: string}>
 }) => {
   const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: {_id: 1, name: 'Abuki'},
+    _id: 1,
+    description: "This is a description",
+    image: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*9HY0dyZdTkryuRgkCiJjxQ.jpeg",
+    category: "Robot",
+    title: "We Robots"
+  }]
   return (
     <>
       <section className='pink_container'>
@@ -16,6 +27,22 @@ const Home = async ({ searchParams }: {
         </p>
 
         <SearchForm query={query}/>
+      </section>
+
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Search result for "${query}"` : 'All Startups'}
+        </p>
+        <ul className='mt-7 card_grid'>
+          {posts?.length > 0 ? (
+           posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post}/>
+           ))
+          ): (
+            <p className='no-results'>No startups found</p>
+          )}
+          
+        </ul>
       </section>
     </>
   )
